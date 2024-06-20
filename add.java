87c4f0e0a24de9f8b0cf7e7d33996cf96af2d9f7
ShrehithaD <158747464+ShrehithaD@users.noreply.github.com>
2024-06-20 14:05:28 +0530

    Create add.java

---
+++ add.java
@@ -0,0 +1,13 @@
+class Add
+ {
+    public static void main(String[] args)
+{
+	int a,b,c;
+	a=78;
+	b=99;
+	c=a+b;
+        System.out.println(c);
+	System.out.println("Sum="+c);
+	System.out.println("Value of a="+a+"Value of b="+b+"Sum="+c);
+    }
+}
